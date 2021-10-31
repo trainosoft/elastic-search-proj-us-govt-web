1b6769cce5995609973e834577841acb6394535f
Sumit.vadaviya <sg.vadaviya@gmail.com>
2021-10-31 17:00:43 +0530

    Updated configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,8 @@ export const environment = {
   appVersion: 'v8.0.27',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'http://143.198.12.169:8080'
+  apiUrl: 'http://143.198.12.169:8080/npi',
+  //apiUrl: 'http://localhost:8081/npi'
 };
 
 /*
