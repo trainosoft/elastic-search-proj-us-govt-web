be5c57f6669fc520d4ead44d18a2fd5cab765977
jay.patel <jay.patel@brainvire.com>
2021-10-31 23:20:36 +0530

    update edit details page

--- src/app/config/config.constant.ts
+++ src/app/config/config.constant.ts
@@ -3,10 +3,47 @@ export const CONFIG_CONSTANTS = {
         NPI: 'npi',
         FIRST_NAME: 'provider_first_name',
         LAST_NAME: 'provider_last_name_(legal_name)',
+        MIDDLE_NAME: 'provider_middle_name',
         DEACTIVATION_DATE: 'npi_deactivation_date',
+        ADDRESS_LINE_1: 'provider_first_line_business_practice_location_address',
+        ADDRESS_LINE_2: 'provider_second_line_business_practice_location_address',
+        TELEPHONE: 'provider_business_practice_location_address_telephone_number',
+        POST_CODE: 'provider_business_practice_location_address_postal_code',
+        FAX_NUMBER: 'provider_business_practice_location_address_fax_number',
         COUNTRY: 'provider_business_practice_location_address_country_code_(if_outside_u.s.)',
         CITY: 'provider_business_practice_location_address_city_name',
         STATE: 'provider_business_practice_location_address_state_name',
+        ENTITY_TYPE_CODE: 'entity_type_code',
+        TELEPHONE_NUMBE: 'provider_business_practice_location_address_telephone_number',
+        LICENCE_NUMBER: 'provider_license_number_1',
+        GENDER: 'provider_gender_code',
+        ENUMERATION_DATE: "provider_enumeration_date",
+        LAST_UPDATED_DATE: "last_update_date",
+        TEXONOMIES: [
+            'healthcare_provider_taxonomy_code_1',
+            'healthcare_provider_taxonomy_code_2',
+            'healthcare_provider_taxonomy_code_3',
+            'healthcare_provider_taxonomy_code_4',
+            'healthcare_provider_taxonomy_code_5',
+            'healthcare_provider_taxonomy_code_6',
+            'healthcare_provider_taxonomy_code_7',
+            'healthcare_provider_taxonomy_code_8',
+            'healthcare_provider_taxonomy_code_9',
+            'healthcare_provider_taxonomy_code_10',
+            'healthcare_provider_taxonomy_code_11',
+            'healthcare_provider_taxonomy_code_12',
+            'healthcare_provider_taxonomy_code_13',
+            'healthcare_provider_taxonomy_code_14',
+            'healthcare_provider_taxonomy_code_15'
+        ],
+        BUSINESS_ADDRESS_LINE_1: 'provider_first_line_business_mailing_address',
+        BUSINESS_ADDRESS_LINE_2: 'provider_second_line_business_mailing_address',
+        BUSINESS_TELEPHONE: 'provider_business_mailing_address_telephone_number',
+        BUSINESS_POST_CODE: 'provider_business_mailing_address_postal_code',
+        BUSINESS_FAX_NUMBER: 'provider_business_mailing_address_fax_number',
+        BUSINESS_COUNTRY: 'provider_business_mailing_address_country_code_(if_outside_u.s.)',
+        BUSINESS_CITY: 'provider_business_mailing_address_city_name',
+        BUSINESS_STATE: 'provider_business_mailing_address_state_name'
     },
     STATUS: {
         ALL: {
@@ -21,5 +58,9 @@ export const CONFIG_CONSTANTS = {
             label: "Active",
             value: "ACTIVE" 
         },
+    },
+    ENTITY_TYPE: {
+        1: 'Individual',
+        2: 'Organization'
     }
 }
--- src/app/modules/search/search-details/search-details.component.ts
+++ src/app/modules/search/search-details/search-details.component.ts
@@ -1,3 +1,4 @@
+import { isEmpty } from './../../../utils/common.utils';
 import { CONFIG_CONSTANTS } from './../../../config/config.constant';
 import { SearchService } from './../../../_services/search.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -11,21 +12,77 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
   styleUrls: ['./search-details.component.scss']
 })
 export class SearchDetailsComponent implements OnInit {
-
+  public isEmpty = isEmpty;
   constructor(private route: ActivatedRoute, private searchService: SearchService, private toastrService: ToastrService, private router: Router,
     private loader: NgxUiLoaderService) { }
-  data: any = {};
-  keys = CONFIG_CONSTANTS.API_DATA_KEYS;
+    data: any = {};
+    keys = CONFIG_CONSTANTS.API_DATA_KEYS;
+    entityTypesTitle: any = CONFIG_CONSTANTS.ENTITY_TYPE;
+    stateMap:{[key:string]: any} = {};
+    countryMap:{[key:string]: any} = {};
+  
   async ngOnInit() {
     this.loader.start();
-    const data: any = await this.searchService.getDetails(this.route.snapshot.params.id).toPromise();
-    if (data.total <=0) {
+    try {
+      const result = await Promise.all([
+        this.searchService.getDetails(this.route.snapshot.params.id).toPromise(),
+        this.searchService.getAllCountriesList().toPromise(),
+        this.searchService.getStateList(null, true).toPromise()
+      ]);
+      const data: any = result[0];
+      if (data.total <=0) {
+        this.toastrService.error("No Record Found");
+        this.loader.stopAll();
+        this.router.navigateByUrl('');
+      }
+      this.data = data.data[0];
+      (result[1] as any).data.forEach((e: any) => {
+        this.countryMap[e.iso2]=e.name;
+      });
+      (result[2] as any).data.forEach((e: any) => {
+        if (!this.stateMap[e['country_code']]) {
+          this.stateMap[e['country_code']]={};
+        }
+        this.stateMap[e['country_code']][e['state_code']]=e.name;
+      });
+      this.loader.stopAll();
+    } catch(err) {
       this.toastrService.error("No Record Found");
       this.loader.stopAll();
       this.router.navigateByUrl('');
     }
-    this.data = data.data[0];
-    this.loader.stopAll();
   }
-
+  getFormattedAddress(data: any[]) {
+    return data.filter(e => !isEmpty(e)).join(", ");
+  }
+  getAddress(address1: any, address2: any,city: any, state: any, country: any, postcode: any) {
+    const add = [];
+    if (address1) {
+      add.push(address1);
+    }
+    if (address2) {
+      add.push(address2);
+    }
+    if (city) {
+      add.push(city);
+    }
+    if (state && country) {
+      if (this.stateMap[country] && this.stateMap[country][state]) {
+        add.push(this.stateMap[country][state]);
+      } else {
+        add.push(state);
+      }
+    }
+    if (country) {
+      add.push(this.countryMap[country]);
+    }
+    if (postcode) {
+      add.push(postcode);
+    }
+    return this.getFormattedAddress(add);
+  }
+  get texonomyData(): any[] {
+    return [...new Set(this.keys.TEXONOMIES.filter(e => !isEmpty(this.data[e])).map(e => this.data[e]))];
+    
+  }
 }
